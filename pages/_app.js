e17024b884f1db4cfd53fd3e1558bba20e46d4b4
SYHHam <halqiilham@gmail.com>
2025-12-07 09:53:23 +0700

    _app.js

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// FILE: pages/_app.js
+// Copy this entire code to your GitHub repository in: pages/_app.js
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
